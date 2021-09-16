4db805e9c10a603e649c5c524c28a361a2ffd9b0
DanielAraujo <daniel.araujos22004@gmail.com>
2021-09-16 00:27:13 -0300

    feitooo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.get('/produto', async(req, resp) => {
 try{
-    let r = await db.tb_produto.findAll()
+    let r = await db.tb_produto.findAll( {order: [['id_produto' , 'desc']]})
     resp.send(r);
 }catch(e){
     resp.send(e.toString())
@@ -18,7 +18,32 @@ try{
 app.post('/produto', async(req, resp) =>  {
 try{
 
-    let {produto, categoria, precoDe, precoPor, avaliacao, produtoDesc , estoque, imgProduto } = req.body;
+    let {produto, categoria, precoDe, precoPor, avaliacao, produtoDesc , estoque, imgProduto} = req.body;
+
+    let nomeRepetido = await db.tb_produto.findOne({ where: {nm_produto: produto}});
+        if(nomeRepetido != null){
+            return resp.send({erro: 'Produto ja cadastrado!'})
+        }
+
+    if(isNaN(avaliacao) || avaliacao <= 0 ){
+        return resp.send({erro: 'Campo "Avaliação" Invalido'})
+    }
+
+    if(isNaN(precoPor) || precoPor <= 0){
+        return resp.send({erro: 'Campo "Preço POR" Invalido'})
+    }
+
+    if(isNaN(precoDe) || precoDe <= 0){
+        return resp.send({erro: 'Campo "Preço DE" Invalido'})
+    }
+
+    if(isNaN(estoque) || estoque <= 0){
+        return resp.send({erro: 'Campo "Estoque" Invalido'})
+    }
+
+    if(produto, categoria, imgProduto, produtoDesc == ''){
+        return resp.send({erro: 'Todos os campos devem ser preenchidos!'})
+    }
 
     let r = await db.tb_produto.create({
         nm_produto: produto,
@@ -28,7 +53,10 @@ try{
         vl_avaliacao: avaliacao,
         ds_produto: produtoDesc,
         qtd_estoque: estoque,
-        img_produto: imgProduto
+        img_produto: imgProduto,
+        bt_ativo: true,
+        dt_inclusao: new Date()
+
     })
     resp.send(r)
 } catch(e){
@@ -41,6 +69,29 @@ app.put('/produto/:id', async(req, resp) => {
         let {id} = req.params;
 
         let {produto, categoria, precoDe, precoPor, avaliacao, produtoDesc, estoque, imgProduto} = req.body;
+
+        
+        if(isNaN(avaliacao) || avaliacao <= 0 ){
+            return resp.send({erro: 'Campo "Avaliação" Invalido'})
+        }
+
+        if(isNaN(precoPor) || precoPor <= 0){
+            return resp.send({erro: 'Campo "Preço POR" Invalido'})
+        }
+
+        if(isNaN(precoDe) || precoDe <= 0){
+            return resp.send({erro: 'Campo "Preço DE" Invalido'})
+        }
+
+        if(isNaN(estoque) || estoque <= 0){
+            return resp.send({erro: 'Campo "Estoque" Invalido'})
+        }
+
+        if(produto, categoria, imgProduto, produtoDesc == ''){
+            return resp.send({erro: 'Todos os campos devem ser preenchidos!'})
+        }
+
+
         
         let r = await db.tb_produto.update({
             nm_produto: produto,
